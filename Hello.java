8ce19b683a089f796218f86e60755ef889a4b6a7
merunhowlader <merunhowlader@gmail.com>
2025-03-08 03:01:37 +0600

    start

---
+++ Hello.java
@@ -0,0 +1,25 @@
+
+class Hello {
+
+    public static void main(String a[]) {
+
+        // int num1 = 5;
+        // int num2 = 6;
+        // int result = num1 + num2;
+        //   System.out.println(num1 + num2);
+        // float marks = 4 + 5;
+        // System.out.print("Hello merun");
+        // System.out.println(marks);
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = 'a';
+        boolean b = true;
+
+    }
+}
