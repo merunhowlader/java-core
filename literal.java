96f108a6b43391129c2cab57054a3e46d440401d
merunhowlader <merunhowlader@gmail.com>
2025-03-08 15:10:39 +0600

    literal in java

---
+++ literal.java
@@ -0,0 +1,30 @@
+
+class literal {
+
+    public static void main(String a[]) {
+        //literal
+
+        ///byanary value 0b \
+        ///In Data or Representation: A literal can refer to a fixed value in programming or mathematics, such as the number 5 or the string "hello".
+     
+        int num1 = 0b111;
+        System.out.println(num1);
+        //hexsa decimal 0x
+        int num2 = 0xA1;
+        System.out.println(num2);
+
+        //you can use unser score between number for easy to count 
+        int num3 = 10_00_000;
+        System.out.println(num3);
+
+        //double
+        double num4 = 10_00_000;
+        System.out.println(num4);
+        //character
+        char c = 'z';
+        System.out.println(c);
+
+        c++;
+        System.out.println(c);
+    }
+}
